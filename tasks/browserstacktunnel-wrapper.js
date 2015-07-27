e58bd4e2b3eb53823c4baa410af9336da3b9cadc
Jan Florian Dietrich <jfd@daenen4.de>
2015-07-27 11:40:56 +0200

    Initial project setup

--- tasks/browserstacktunnel-wrapper.js
+++ tasks/browserstacktunnel-wrapper.js
@@ -6,7 +6,52 @@
 module.exports = function(grunt) {
   'use strict';
 
-  grunt.registerMultiTask('browserstacktunnel-wrapper', 'A grunt plugin around browserstacktunnel-wrapper', function() {
+  require('global-tunnel').initialize();
 
+  var
+    BrowserStackTunnel = require('browserstacktunnel-wrapper'),
+    // sharing this object will only work for sharing it in the same grunt call
+    _tunnel = {};
+
+  grunt.registerTask('browserstacktunnel-wrapper', 'Start a BrowserstackTunnel for local testing', function() {
+    var options = this.options({
+        key: '',
+        hosts: [
+            {
+              name: 'localhost',
+              port: 3500,
+              sslFlag: 0
+            }
+        ],
+        verbose: false
+      }),
+      done = this.async();
+
+    _tunnel = new BrowserStackTunnel(options);
+
+    grunt.log.debug('Establishing tunnel')
+    _tunnel.start(function(error) {
+      if (error) {
+        grunt.log.errorlns('Tunnel could not be started');
+        done(error);
+      } else {
+        grunt.log.oklns('BrowserStackLocalTunnel established');
+        done();
+      }
+    });
+  });
+
+  grunt.registerTask('browserstacktunnel-wrapper:stop', 'Stop the Browserstacktunnel', function() {
+    var done = this.async();
+    _tunnel.stop(function(error) {
+      if (error) {
+        grunt.log.errorlns('Tunnel could not be started');
+        grunt.log.error(error);
+        done(error);
+      } else {
+        grunt.log.oklns('BrowserStackLocalTunnel closed');
+        done();
+      }
+    });
   });
 };
